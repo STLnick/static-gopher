401e69c086d5ebd30bb6176fb26bf715305d8f44
Nick Ray <nickrray@gmail.com>
2024-07-06 17:02:07 -0500

    Generate HTML from markdown string

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func main() {
@@ -25,9 +26,7 @@ func main() {
 	fmt.Println(sourceDir, destDir, templateDir)
 
 	textNodes := TextToTextNodes("This has *italic* text.")
-	for _, tn := range textNodes {
-		fmt.Println(tn)
-	}
+    _ = textNodes
 }
 
 type BlockType int
@@ -61,6 +60,53 @@ func (bt BlockType) Name() string {
 		return "ORDERED_LIST"
 	}
 	return "INVALID"
+}
+
+type HTMLNode struct {
+	tag      string     // HTML tag
+	props    string     // Props to add to element TODO
+	children []HTMLNode // HTML children
+	value    string     // text
+}
+
+func (h HTMLNode) String() string {
+	if h.children == nil {
+		return fmt.Sprintf("LeafNode{\"<%s>\", props=%s, value=\"%s\"}", h.tag, h.props, h.value)
+	} else {
+		return fmt.Sprintf("ParentNode{\"<%s>\", props=%s, children=\"%s\"}", h.tag, h.props, h.children)
+	}
+}
+
+func (h HTMLNode) ToHTML() string {
+	if h.children == nil {
+		textNodes := TextToTextNodes(h.value)
+		value := textNodesToHTML(textNodes)
+		return fmt.Sprintf("<%s %s>%s</%s>", h.tag, h.props, value, h.tag)
+	}
+
+	var childHTML string
+	for _, child := range h.children {
+		childHTML += child.ToHTML()
+	}
+	return fmt.Sprintf("<%s %s>%s</%s>", h.tag, h.props, childHTML, h.tag)
+}
+
+func NewLeafNode(tag string, props string, value string) HTMLNode {
+	return HTMLNode{
+		tag:      tag,
+		props:    props,
+		children: nil,
+		value:    value,
+	}
+}
+
+func NewParentNode(tag string, props string, children []HTMLNode) HTMLNode {
+	return HTMLNode{
+		tag:      tag,
+		props:    props,
+		children: children,
+		value:    "",
+	}
 }
 
 type TextType int
@@ -106,6 +152,55 @@ func (t TextNode) String() string {
 	return fmt.Sprintf("TextNode{\"%s\", %s, url=\"%s\"}", t.text, t.textType.Name(), t.url)
 }
 
+func (t TextNode) Tag() string {
+	if t.textType == TEXT {
+		return ""
+	}
+	if t.textType == BOLD {
+		return "b"
+	}
+	if t.textType == ITALIC {
+		return "i"
+	}
+	if t.textType == CODE_TEXT {
+		return "code"
+	}
+	if t.textType == LINK {
+		return "a"
+	}
+	if t.textType == IMAGE {
+		return "img"
+	}
+	return ""
+}
+
+func (t TextNode) ToHTML() string {
+	switch t.textType.Name() {
+	case "BOLD":
+		return fmt.Sprintf("<b>%s</b>", t.text)
+	case "ITALIC":
+		return fmt.Sprintf("<i>%s</i>", t.text)
+	case "CODE_TEXT":
+		return fmt.Sprintf("<code>%s</code>", t.text)
+	case "LINK":
+		return fmt.Sprintf("<a href=\"%s\">%s</a>", t.url, t.text)
+	case "IMAGE":
+		return fmt.Sprintf("<img src=\"%s\" alt=\"%s\" />", t.url, t.text)
+	default:
+		return t.text
+	}
+
+	return fmt.Sprintf("")
+}
+
+func textNodesToHTML(nodes []TextNode) string {
+	var html string
+	for _, node := range nodes {
+		html += node.ToHTML()
+	}
+	return html
+}
+
 func NewTextNode(text string, textType TextType, url string) TextNode {
 	return TextNode{
 		text:     text,
@@ -121,21 +216,25 @@ func TextToTextNodes(text string) []TextNode {
 	if err != nil {
 		log.Panic("splitting on bold:", err)
 	}
-	nodes, err = splitNodesDelimiter(nodes, "*")
+	
+    nodes, err = splitNodesDelimiter(nodes, "*")
 	if err != nil {
 		log.Panic("splitting on italic:", err)
 	}
-	nodes, err = splitNodesDelimiter(nodes, "`")
+	
+    nodes, err = splitNodesDelimiter(nodes, "`")
 	if err != nil {
 		log.Panic("splitting on code:", err)
 	}
-	nodes, err = splitNodesLinks(nodes)
-	if err != nil {
-		log.Panic("splitting on links:", err)
-	}
-	nodes, err = splitNodesImages(nodes)
+	
+    nodes, err = splitNodesImages(nodes)
 	if err != nil {
 		log.Panic("splitting on images:", err)
+	}
+	
+    nodes, err = splitNodesLinks(nodes)
+    if err != nil {
+		log.Panic("splitting on links:", err)
 	}
 
 	return nodes
@@ -163,44 +262,242 @@ func splitNodesDelimiter(nodes []TextNode, delimiter string) ([]TextNode, error)
 	}
 
 	for _, node := range nodes {
-        newNodes = append(newNodes, splitter(re, targetType, node)...)
+		newNodes = append(newNodes, splitter(re, targetType, node)...)
 	}
 
 	return newNodes, nil
 }
 
 func splitter(re *regexp.Regexp, targetType TextType, node TextNode) []TextNode {
-    result := re.FindStringSubmatch(node.text)
+	result := re.FindStringSubmatch(node.text)
 
-    if result == nil {
-        return []TextNode{node}
-    }
+	if result == nil {
+		return []TextNode{node}
+	}
 
-    var splitNodes []TextNode
+	var splitNodes []TextNode
 
-    pre := result[1]
-    if pre != "" {
-        tempNode := NewTextNode(pre, node.textType, node.url)
-        splitNodes = append(splitNodes, splitter(re, targetType, tempNode)...)
-    }
+	pre := result[1]
+	if pre != "" {
+		tempNode := NewTextNode(pre, node.textType, node.url)
+		splitNodes = append(splitNodes, splitter(re, targetType, tempNode)...)
+	}
 
-    targetText := result[2]
-    splitNodes = append(splitNodes, NewTextNode(targetText, targetType, ""))
+	targetText := result[2]
+	splitNodes = append(splitNodes, NewTextNode(targetText, targetType, ""))
 
-    post := result[3]
-    if post != "" {
-        splitNodes = append(splitNodes, NewTextNode(post, node.textType, node.url))
-    }
+	post := result[3]
+	if post != "" {
+		splitNodes = append(splitNodes, NewTextNode(post, node.textType, node.url))
+	}
 
-    return splitNodes
+	return splitNodes
 }
 
 func splitNodesLinks(nodes []TextNode) ([]TextNode, error) {
-	// TODO
-	return nodes, nil
+    var splitNodes []TextNode
+    linkPattern := regexp.MustCompile(`(.*)\[([^\[\]\(\)]+)\]\(([^\[\]\(\)]+)\)(.*)`)
+
+    for _, node := range nodes {
+        result := linkPattern.FindStringSubmatch(node.text)
+
+        if result == nil {
+            splitNodes = append(splitNodes, node)
+            continue
+        }
+
+        pre := result[1]
+        if pre != "" {
+            tempNode := NewTextNode(pre, node.textType, node.url)
+            preNodes, _ := splitNodesLinks([]TextNode{tempNode})
+            splitNodes = append(splitNodes, preNodes...)
+        }
+
+        text := result[2]
+        url := result[3]
+        splitNodes = append(splitNodes, NewTextNode(text, LINK, url))
+
+        post := result[4]
+        if post != "" {
+            splitNodes = append(splitNodes, NewTextNode(post, node.textType, node.url))
+        }
+    }
+
+	return splitNodes, nil
 }
 
 func splitNodesImages(nodes []TextNode) ([]TextNode, error) {
-	// TODO
-	return nodes, nil
+    var splitNodes []TextNode
+    imgPattern := regexp.MustCompile(`(.*)\!\[([^\[\]\(\)]+)\]\(([^\[\]\(\)]+)\)(.*)`)
+
+    for _, node := range nodes {
+        result := imgPattern.FindStringSubmatch(node.text)
+
+        if result == nil {
+            splitNodes = append(splitNodes, node)
+            continue
+        }
+
+        pre := result[1]
+        if pre != "" {
+            tempNode := NewTextNode(pre, node.textType, node.url)
+            preNodes, _ := splitNodesImages([]TextNode{tempNode})
+            splitNodes = append(splitNodes, preNodes...)
+        }
+
+        text := result[2]
+        url := result[3]
+        splitNodes = append(splitNodes, NewTextNode(text, IMAGE, url))
+
+        post := result[4]
+        if post != "" {
+            splitNodes = append(splitNodes, NewTextNode(post, node.textType, node.url))
+        }
+    }
+
+	return splitNodes, nil
+}
+
+func getBlockType(blockText string) BlockType {
+	headingPattern := regexp.MustCompile(`#+ (.*)`)
+	result := headingPattern.FindStringSubmatch(blockText)
+	if result != nil {
+		return HEADING
+	}
+
+	if strings.HasPrefix(blockText, "```") && strings.HasSuffix(blockText, "```") {
+		return CODE
+	}
+
+	isQuote := true
+	for _, line := range strings.Split(blockText, "\n") {
+		if !strings.HasPrefix(line, "> ") {
+			isQuote = false
+			break
+		}
+	}
+	if isQuote {
+		return QUOTE
+	}
+
+	isUl := true
+	for _, line := range strings.Split(blockText, "\n") {
+		if !strings.HasPrefix(line, "* ") && !strings.HasPrefix(blockText, "- ") {
+			isUl = false
+			break
+		}
+	}
+	if isUl {
+		return UNORDERED_LIST
+	}
+
+	isOl := true
+	index := 1
+	for _, line := range strings.Split(blockText, "\n") {
+		if !strings.HasPrefix(line, fmt.Sprintf("%d. ", index)) {
+			isOl = false
+			break
+		}
+		index++
+	}
+	if isOl {
+		return ORDERED_LIST
+	}
+
+	return PARAGRAPH
+}
+
+func MarkdownToBlocks(md string) []string {
+	var textBlocks []string
+	var current []string
+
+	for _, chunk := range strings.Split(md, "\n") {
+		if chunk != "" {
+			current = append(current, chunk)
+		} else {
+			if current != nil {
+				textBlocks = append(textBlocks, strings.Join(current, "\n"))
+				current = nil
+			}
+		}
+	}
+
+	if current != nil {
+		textBlocks = append(textBlocks, strings.Join(current, "\n"))
+		current = nil
+	}
+
+	return textBlocks
+}
+
+func BlocksToHTML(textBlocks []string) string {
+	var nodes []HTMLNode
+	for _, textBlock := range textBlocks {
+		var node HTMLNode
+
+		switch bt := getBlockType(textBlock); bt {
+		case PARAGRAPH:
+			node = createParagraph(textBlock)
+		case CODE:
+			node = createCode(textBlock)
+		case HEADING:
+			node = createHeading(textBlock)
+		case QUOTE:
+			node = createQuote(textBlock)
+		case UNORDERED_LIST:
+			node = createUnorderedList(textBlock)
+		case ORDERED_LIST:
+			node = createOrderedList(textBlock)
+		default:
+			log.Panic(fmt.Sprint("converting md to block - unexpected block type: ", bt))
+		}
+
+		nodes = append(nodes, node)
+	}
+
+	root := NewParentNode("div", "", nodes)
+	return root.ToHTML()
+}
+
+func createHeading(text string) HTMLNode {
+	idx := strings.Index(text, " ")
+	tag := fmt.Sprintf("h%d", idx)
+	value := text[idx+1:]
+
+	return NewLeafNode(tag, "", value)
+}
+
+func createParagraph(text string) HTMLNode {
+	return NewLeafNode("p", "", text)
+}
+
+func createCode(text string) HTMLNode {
+	code := NewLeafNode("code", "", text[3:len(text)-3])
+	return NewParentNode("pre", "", []HTMLNode{code})
+}
+
+func createQuote(text string) HTMLNode {
+	var value []string
+	for _, line := range strings.Split(text, "\n") {
+		value = append(value, line[2:])
+	}
+	return NewLeafNode("blockquote", "", strings.Join(value, "\n"))
+}
+
+func createUnorderedList(text string) HTMLNode {
+	var listItems []HTMLNode
+	for _, line := range strings.Split(text, "\n") {
+		item := NewLeafNode("li", "", line[2:])
+		listItems = append(listItems, item)
+	}
+	return NewParentNode("ul", "", listItems)
+}
+
+func createOrderedList(text string) HTMLNode {
+	var listItems []HTMLNode
+	for _, line := range strings.Split(text, "\n") {
+		item := NewLeafNode("li", "", line[3:])
+		listItems = append(listItems, item)
+	}
+	return NewParentNode("ol", "", listItems)
 }
--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "testing"
+    "strings"
 )
 
 type TextToTextNodesTest struct {
@@ -10,7 +11,7 @@ type TextToTextNodesTest struct {
     typeCounts map[TextType]int
 }
 
-var tests = []TextToTextNodesTest{
+var textToTextNodeTests = []TextToTextNodesTest{
     TextToTextNodesTest{
         "This has *italic text*, **bold** text, and `a little bit of code`.",
         7,
@@ -41,23 +42,35 @@ var tests = []TextToTextNodesTest{
             CODE_TEXT: 2,
         },
     },
+    TextToTextNodesTest{
+        "[i am link](https://www.google.com)",
+        1,
+        map[TextType]int{
+            LINK: 1,
+        },
+    },
+    TextToTextNodesTest{
+        "![i am image](https://picsum.photos/200/300)",
+        1,
+        map[TextType]int{
+            IMAGE: 1,
+        },
+    },
 }
 
 func TestTextToTextNodes(t *testing.T) {
-    for _, test := range tests {
+    for _, test := range textToTextNodeTests {
         textNodes := TextToTextNodes(test.text)
         if len(textNodes) != test.expectedLen {
-            for i, n := range textNodes {
-                t.Log(i, n)
-            }
             t.Errorf("Expected (%d) nodes but got (%d)\n", test.expectedLen, len(textNodes))
         }
         counts := map[TextType]int{
-
             TEXT: 0,
             BOLD: 0,
             ITALIC: 0,
             CODE_TEXT: 0,
+            LINK: 0,
+            IMAGE: 0,
         }
         for _, node := range textNodes {
             counts[node.textType] += 1
@@ -69,3 +82,66 @@ func TestTextToTextNodes(t *testing.T) {
         }
     }
 }
+
+type MarkdownToTextBlockTest struct {
+    md string
+    expectedBlocks int
+}
+
+var markdownToTextBlockTests = []MarkdownToTextBlockTest{
+    MarkdownToTextBlockTest{
+        "# heading1\n\ntext\n\n## heading2\n\n\n> q1\n> q2\n\n- li1\n- li2\n\n1. li1\n2. li2",
+        6,
+    },
+    MarkdownToTextBlockTest{
+        "```\nconst x = 2;\n```\n\n## heading 2\n\njust some text",
+        3,
+    },
+    MarkdownToTextBlockTest{
+        "# heading1\n\nthis text has an ![image](https://picsum.photos/300/200) and a [link](https://www.google.com)",
+        2,
+    },
+}
+
+func TestMarkdownToTextBlocks(t *testing.T) {
+    for _, test := range markdownToTextBlockTests {
+        results := MarkdownToBlocks(test.md)
+        if len(results) != test.expectedBlocks {
+            t.Errorf("Expected (%d) blocks but got (%d)", test.expectedBlocks, len(results))
+        }
+    }
+}
+
+type BlocksToHTMLTest struct {
+    md string
+    expectedTags []string
+}
+
+var blocksToHTMLTests = []BlocksToHTMLTest{
+    BlocksToHTMLTest{
+        "# heading1\n\ntext\n\n## heading2\n\n\n> q1\n> q2\n\n- li1\n- li2\n\n1. li1\n2. li2",
+        []string{"div", "h1", "p", "h2", "blockquote", "ul", "ol", "li"},
+    },
+    BlocksToHTMLTest{
+        "```\nconst x = 2;\n```\n\n## heading 2\n\njust some text",
+        []string{"div", "pre", "code", "h2", "p"},
+    },
+    BlocksToHTMLTest{
+        "# heading1\n\nthis text has an ![image](https://picsum.photos/300/200) and a [link](https://www.google.com)",
+        []string{"div", "h1", "img", "a", "p"},
+    },
+}
+
+func TestBlocksToHTML(t *testing.T) {
+    for _, test := range blocksToHTMLTests {
+        blocks := MarkdownToBlocks(test.md)
+        result := BlocksToHTML(blocks)
+        for _, tag := range test.expectedTags {
+            if strings.Index(result, tag) == -1 {
+                t.Errorf("Expected (%s) tag but was not present", tag)
+            }
+        }
+    }
+}
+
+
